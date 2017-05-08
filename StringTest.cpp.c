a6d3a41ba90db7917da6c43243c02ca62aa0b07c
likailongds <2071230564@qq.com>
2017-05-08 17:51:27 +0800

    123

---
+++ StringTest.cpp.c
@@ -0,0 +1,44 @@
+# include <iostream>
+# include <string>
+# include <set>
+using namespace std;
+int findMaxLength(string str)
+{
+	int len = str.length();
+	if (len <= 1)
+		return len;
+	int cnt = 1, begin = 0, end = 1;
+	set<char> s;
+	s.insert(str[0]);
+	while (begin < len && end < len)
+	{
+		if (s.find(str[end]) == s.end())
+		{
+			s.insert(str[end]);
+			end++;
+		}
+		else
+		{
+			if (cnt < (end - begin))
+				cnt = end - begin;
+			int pos = begin;
+			while (str[pos] != str[end])
+			{
+				s.erase(str[pos]);
+				pos++;
+				
+			}
+				
+			begin = pos + 1;
+			end++;
+		}
+	}
+	return cnt;
+}
+int main()
+{
+	string str;
+	cin >> str;
+	cout << findMaxLength(str) << endl;
+	return 0;
+}
